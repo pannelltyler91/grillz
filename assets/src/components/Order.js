a3a08efe74ab43129d60a3093009f4e0368a28c7
Tyler Pannell <pannelltyler91@gmail.com>
2022-04-25 19:13:45 -0400

    working on reducer functionality

--- assets/src/components/Order.js
+++ assets/src/components/Order.js
@@ -1,76 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { useSelector, useDispatch} from 'react-redux'
-import { StyleSheet, Text, View, TextInput, Button, } from 'react-native';
-import {selectBottomTeeth,selectDesign,selectMetal,selectTopTeeth,setCustomer,checkout} from '../features/order'
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  selectBottomTeeth,
+  selectDesign,
+  selectKarat,
+  selectTopTeeth,
+  checkout,
+  selectMetal,
+} from "../features/order";
 
 export default function Order() {
-  const price = useSelector((state) => state.pricing.total)
-  const metalList = useSelector((state) => state.pricing.metals)
-  const metal = useSelector((state) => state.order.metal)
-  const design = useSelector((state) => state.order.design)
-  const topCount = useSelector((state) => state.order.topCount)
-  const bottomCount = useSelector((state) => state.order.bottomCount)
-  const customerInfo = useSelector((state) => state.order.customerInfo)
-
+  const price = useSelector((state) => state.order.total);
+  const karatList = useSelector((state) => state.pricing.karats);
+  const topTeeth = useSelector((state) => state.pricing.topTeeth);
+  const bottomTeeth = useSelector((state) => state.pricing.bottomTeeth);
+  const designList = useSelector((state) => state.pricing.designs);
+  const metalList = useSelector((state) => state.pricing.metals);
   const dispatch = useDispatch();
+  const [phone,setPhone] = useState('');
+  const [name,setName] = useState('');
   return (
-    
     <View style={styles.container}>
-      <View ><Text style={styles.header}>Homeboy</Text></View>
-      <View style={styles.metals} name='metals'>
-
-        {metalList.map((metal) => {
-            return(
-                <button key={metal} onTouchStart={() => {dispatch(selectMetal({metal:{metal}}))}}><Text>{metal}</Text></button>
-            )
+      <View>
+        <Text style={styles.header}>Homeboy</Text>
+      </View>
+      <View style={styles.metals} name="karats">
+        {karatList.map((karat) => {
+          return (
+            <button
+              key={karat}
+              onTouchStart={() => {
+                dispatch(selectKarat( {karat:{ karat } }));
+              }}
+            >
+              <Text>{karat}</Text>
+            </button>
+          );
         })}
-
       </View>
-      <View name='teeth'>
-        <View name='top' style={styles.rows}>
-          
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
+      <View name="teeth">
+        <View name="top" style={styles.rows}>
+          {topTeeth.map((tooth) => {
+            return (
+              <button
+                key={tooth}
+                onTouchStart={() => {
+                  dispatch(selectTopTeeth({tooth:{tooth}}))
+                }}
+                style={{ width: "25px", height: "25px" }}
+              ></button>
+            );
+          })}
         </View>
-        <View name='bottom' style={styles.rows}>
-          <button onTouchStart={() => {console.log('tooth clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
-          <button onTouchStart={() => {console.log('clicked')}} style={{width:'25px',height:'25px'}}></button>
+        <View name="bottom" style={styles.rows}>
+          {bottomTeeth.map((tooth) => {
+            return (
+              <button
+                key={tooth}
+                onTouchStart={() => {
+                  dispatch(selectBottomTeeth({tooth:{tooth}}))
+                }}
+                style={{ width: "25px", height: "25px" }}
+              ></button>
+            );
+          })}
         </View>
       </View>
-      <View style={styles.designs} name='designs'>
-        <button>Open Face</button>
-        <button>Diamond Cut</button>
-        <button>Diamond Dust</button>
+      <View style={styles.textholder}>
+          {metalList.map((metal) =>{
+              return(
+                  <button onTouchStart={() => dispatch(selectMetal({metal:{metal}}))} key={metal}>{metal}</button>
+              )
+          })}
+      </View>
+      <View style={styles.designs} name="designs">
+            <View ><Text style={styles.titles}>Pick a Design</Text></View>
+        {designList.map((design) => {
+          return <button onTouchStart={() => {dispatch(selectDesign({design:{design}}))}} key={design}>{design}</button>;
+        })}
       </View>
       <View style={styles.textholder}>
-        <Text style={styles.titles}>Price:${price}</Text>
+        <Text style={styles.titles}>Price:${price === 0? '' : price}</Text>
       </View>
       <View style={styles.textholder}>
-        <Text style={styles.titles}>Name:<TextInput style={styles.inputs}/></Text>
-        <Text style={styles.titles}>Phone:<TextInput style={styles.inputs}/></Text>
+        <Text style={styles.titles}>
+          Name:
+          <TextInput onChange={(e)=>{setName(e.target.value); console.log(name)}}  style={styles.inputs} />
+        </Text>
+        <Text style={styles.titles}>
+          Phone:
+          <TextInput onChange={(e)=>{setPhone(e.target.value); console.log(phone)}}  style={styles.inputs} />
+        </Text>
       </View>
-      <button><Text>Checkout</Text></button>
+      <button>
+        <Text>Checkout</Text>
+      </button>
     </View>
   );
 }
@@ -78,46 +104,48 @@ export default function Order() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#415a77',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#415a77",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  rows:{
-    display:'inline-block',
+  rows: {
+    display: "inline-block",
   },
-  metals:{
-    marginBottom:'20px',
-    display:'inline-block',
+  metals: {
+    marginBottom: "20px",
+    display: "inline-block",
+  },
+  header: {
+    color: "#e0e1dd",
+    fontSize: "50px",
+    fontWeight: "bold",
+    marginBottom: "20px",
+  },
+  designs: {
+    marginTop: "20px",
+    display: "inline-block",
+    border:'solid #e0e1dd 5px ',
+    padding:'10px'
+  },
+  titles: {
+    color: "#e0e1dd",
+    fontSize: "15px",
+    fontWeight: "bold",
+    textAlign:'center',
+    marginTop:'10px',
+    marginBottom:'10px'
 
   },
-  header:{
-    color:'#e0e1dd',
-    fontSize:'50px',
-    fontWeight:'bold',
-    marginBottom:'20px',
+  textholder: {
+    marginTop: "20px",
   },
-  designs:{
-    marginTop:'20px',
-    display:'inline-block',
+  inputs: {
+    border: "2px solid gray",
+    backgroundColor: "white",
+    borderRadius: "5%",
+    margin: "2px",
   },
-  titles:{
-    color:'#e0e1dd',
-    fontSize:'15px',
-    fontWeight:'bold',
-
+  checkoutButton: {
+    marginTop: "20px",
   },
-  textholder:{
-    marginTop:'20px',
-  },
-  inputs:{
-    border:'2px solid gray',
-    backgroundColor:'white',
-    borderRadius:'5%',
-    margin:'2px'
-  },
-  checkoutButton:{
-    marginTop:'20px'
-  }
-
 });
-
--- assets/src/features/order.js
+++ assets/src/features/order.js
@@ -2,8 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const orderSlice = createSlice({
   name: "order",
-  initialState: { metal:'empty', topCount:0,bottomCount:0, design:'empty',customer:{} },
+  initialState: {total:0, order:[] },
   reducers: {
+    selectKarat: (state, action) => {
+      let choice = action.payload
+      console.log(state.order)
+
+    },
     selectMetal: (state, action) => {
       let choice = action.payload
       console.log(choice)
@@ -20,15 +25,12 @@ export const orderSlice = createSlice({
         let choice = action.payload
         console.log(choice)
     },
-    setCustomer:(state,action) =>{
-        let customerInfo = action.payload
-        console.log(customerInfo)
-    },
     checkout:(state,action) =>{
         let order = action.payload
         console.log(order)
-    }
+    },
+
   },
 });
-export const { selectBottomTeeth,selectDesign,selectMetal,selectTopTeeth,setCustomer,checkout } = orderSlice.actions;
+export const { selectBottomTeeth,selectDesign,selectKarat,selectTopTeeth,checkout,selectMetal } = orderSlice.actions;
 export default orderSlice.reducer;
